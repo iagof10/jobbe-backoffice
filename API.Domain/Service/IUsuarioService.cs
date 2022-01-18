b5589a2ad1926e0a7f56a950efe5bec296f25918
VALDEMAR SANTOS <valdemarsaugusto@gmail.com>
2022-01-18 20:46:46 -0300

    Usuarios - criaçao DTO, controllers, interface service

---
+++ API.Domain/Service/IUsuarioService.cs
@@ -0,0 +1,18 @@
+﻿using API.Domain.DTOs;
+using API.Domain.DTOs.Usuario;
+using API.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Service
+{
+    public interface IUsuarioService
+    {
+        Task<bool> ExistAsync(long idUsuario);
+        Task<ResponseBase<IEnumerable<UsuarioDto>>> GetListAsync();
+        Task<ResponseBase<UsuarioDto>> Post(UsuarioCreateInput model);
+    }
+}
---
+++ WebProject/Controllers/UsuarioController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Domain.DTOs.Usuario;
+using API.Domain.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebProject.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private IUsuarioService _service;
+
+        public UsuarioController(IUsuarioService service)
+        {
+            _service = service;
+        }
+
+        // GET: Projects
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext?.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var result = await _service.GetListAsync();
+            if (result.Sucess)
+            {
+                return View(result.Data);
+            }
+            else
+            {
+                return View(new List<UsuarioDto>());
+            }
+        }
+
+        public IActionResult Create()
+        {
+            if (HttpContext?.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Descricao,ImagemUrl")] UsuarioCreateInput model)
+        {
+            if (HttpContext?.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _service.Post(model);
+
+            if (result.Sucess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
